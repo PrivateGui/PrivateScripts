bce3c763242a4427f6e6da84490fb57a02e50951
PrivateGui <110562273+PrivateGui@users.noreply.github.com>
2022-08-03 22:52:31 +0430

    Create BrookHaven.lua

---
+++ BrookHaven.lua
@@ -0,0 +1,69 @@
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+
+local Window = OrionLib:MakeWindow({Name = "Private BrookHaven RP gui", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = true
+})
+
+
+local Section = Tab:AddSection({
+	Name = "Brookhaven RP Main Section"
+})
+
+
+
+Tab:AddButton({
+	Name = "Spawn Car",
+	Callback = function()
+		OrionLib:MakeNotification({
+			Name = "WARNING",
+			Content = "Please do not spawn the car too fast, If doing so then your gonna get kicked.",
+			Image = "rbxassetid://4483345998",
+			Time = 5
+		})
+
+		wait(1.99)
+
+		local args = {
+			[1] = "PickingCar",
+			[2] = "Hearse"
+		}
+		game:GetService("ReplicatedStorage").WW:FindFirstChild("6T6Ca6T6r"):FireServer(unpack(args))		
+  	end    
+})
+
+
+
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		ObSsTu
+  	end    
+})
+
+
+
+
+
+
+
+OrionLib:MakeNotification({
+	Name = "GUI",
+	Content = "Brookhaven Private Gui has been loaded!",
+	Image = "rbxassetid://4483345998",
+	Time = 20
+})
+
+
+
+
+
+
+
+
+OrionLib:Init() --End function

8b3c96fa8b4d7fd5fd44e8d307384d333b360b65
PrivateGui <110562273+PrivateGui@users.noreply.github.com>
2023-02-02 21:48:21 +0330

    Update LuckyBlockProject.lua

--- LuckyBlockProject.lua
+++ LuckyBlockProject.lua
@@ -1,3 +1,5 @@
+--Game link here: https://www.roblox.com/games/662417684/LUCKY-BLOCKS-Battlegrounds
+
 if game.PlaceId == 662417684 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/PrivateGui/PrivateScripts/main/LuckyBlock.lua')))()
 end

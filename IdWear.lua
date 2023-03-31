71a9460883927ad5ca3ee3e526afef293c048619
PrivateGui <110562273+PrivateGui@users.noreply.github.com>
2023-03-31 08:13:28 -0800

    Update IdWear.lua

--- IdWear.lua
+++ IdWear.lua
@@ -1,4 +1,4 @@
 getgenv().ohString1 = "wear"
-getgenv().accessorie = false
+getgenv().accessorie = 
 
 game:GetService("ReplicatedStorage").JJ3["2NN2Updat2NN2eAvata2NN2r"]:FireServer(ohString1, accessorie)
